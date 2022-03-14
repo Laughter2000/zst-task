61fd168062efe6a55cc1feae49dba2a858f3a224
Isaac Amunenwa <isaacamunenwa@gmail.com>
2022-03-14 16:00:22 +0100

    resolved opacity for search bar

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -55,7 +55,7 @@ const Dashboard = () => {
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
             <i className="fas fa-search fill-slate-300 text-gray-400"></i>
             </span>
-            <input  className={`placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-4 pl-9 pr-3 shadow-sm focus:outline-none focus:border-main focus:ring-1 sm:text-sm`} placeholder="Search for company, provider, user, etc." type="text" name="search" onChange={e => setSearchValue(e.target.value)} disabled={!isAuthenticated ? true : false}/>
+            <input  className={`placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-4 pl-9 pr-3 shadow-sm focus:outline-none focus:border-main focus:ring-1 sm:text-sm disabled:opacity-10`} placeholder="Search for company, provider, user, etc." type="text" name="search" onChange={e => setSearchValue(e.target.value)} disabled={!isAuthenticated ? true : false}/>
         </label>
 
 
